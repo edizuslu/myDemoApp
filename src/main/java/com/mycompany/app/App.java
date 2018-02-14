3fd1a24bd527e5222034a2981c012f3c577579a9
Ediz Uslu <edizusluu@gmail.com>
2018-02-14 21:04:17 +0300

    I have started my awesome projectgit statusgit status It will be great.

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,30 @@
+package com.mycompany.app;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+//import junit.framework.TestCase;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+	public static boolean search(ArrayList<Integer> array, int e) {
+        System.out.println("inside search");
+        if (array == null) return false;
+  
+        for (int elt : array) {
+          if (elt == e) return true;
+        }
+        
+        return false;
+    }
+
+
+
+    public static void main( String[] args ){
+        
+        System.out.println( "Hello World!" );
+    }
+}
